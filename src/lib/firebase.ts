06dd7308f7cbff4a3080399217886d4f28a42239
Camillo Vecchione <vecchionecamillo@gmail.com>
2025-09-25 14:30:24 +0000

    Try fixing this error: `Console Error: [2025-09-25T14:29:53.272Z] @fireb

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,6 +10,7 @@ const firebaseConfig: FirebaseOptions = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 };
 
 type FirebaseInstances = {
