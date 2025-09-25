a0984becf2fca6cf3f0389d595f0aa1155d75f0b
Camillo Vecchione <vecchionecamillo@gmail.com>
2025-09-25 12:19:46 +0000

    Try fixing this error: `Console Error: Failed to trigger database seedin

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+      maxDuration: 60,
+    },
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
